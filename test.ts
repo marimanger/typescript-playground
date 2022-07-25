82c25e6047f25e9c7dbf8918002efd427e771a3c
Mariia Bondarenko <MariiaBondarenko@MBAl20C02DJ2QNM6KG.local>
2022-07-25 22:58:35 +0100

    basic-types

--- test.ts
+++ test.ts
@@ -1,1 +1,21 @@
-const a: number = 7;
+const a: number = 5;
+const b = "kkdke";
+const c = true;
+
+let d: string[] = ["a", "b", "c"];
+
+function test(a: string): number | boolean {
+  return true;
+}
+
+const test2 = (a: number): boolean => {
+  return true;
+};
+
+d = d.map((a: string) => a.toLowerCase());
+
+function countCoord(coord: { a: number; b: number }): number {
+  return coord.a + coord.b;
+}
+
+console.log(countCoord({ a: 1, b: 2 }));
