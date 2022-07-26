0ca96a4d083dde12677ad34e55d02f524513a776
marimanger <mariiamanger@gmail.com>
2022-07-26 15:03:13 +0100

    basics types&Interfaces

--- test.ts
+++ test.ts
@@ -19,3 +19,31 @@ function countCoord(coord: { a: number; b: number }): number {
 }
 
 console.log(countCoord({ a: 1, b: 2 }));
+
+//used mostly when we want to make string or number 
+type Point = {
+  x: number;
+  y: number;
+};
+
+//different between interface and type in typescript that you can reassign 
+//and add more properties to interface
+
+//most used 
+interface IPoint {
+  x: number;
+  y: number;
+}
+
+function printPoint(point: Point) {
+  console.log(point.x, point.y);
+}
+
+type stringOrNumber = string | number;
+
+function printStringOrNumber(a: stringOrNumber) {
+  console.log(a);
+}
+
+//casting
+const myCanvas = document.getElementById("myCanvas") as HTMLCanvasElement;
